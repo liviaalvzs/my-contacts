3411a0dc8e33d147667776d93d5a289ca35b2f26
liviaalvzs <livs.santana01@gmail.com>
2022-07-11 13:03:40 -0300

    refatoracao p/ nova versao uuid

--- src/app/repositories/contactRepository.js
+++ src/app/repositories/contactRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Livia',
     email: 'livia@mail.com',
     phone: '1234-4567',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
