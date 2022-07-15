cbc1cad4ac2bba55b526e8390c723c49a983371b
liviaalvzs <livs.santana01@gmail.com>
2022-07-15 19:35:51 -0300

    criacao da rota/controller update contact

--- src/app/controllers/contactController.js
+++ src/app/controllers/contactController.js
@@ -30,7 +30,7 @@ class ContactController {
 
     const contactExists = await contactsRepository.findByEmail(email);
     if (contactExists) {
-      return response.status(400).json({ error: 'this email is already been taken' });
+      return response.status(400).json({ error: 'this email is already in use' });
     }
 
     const contact = await contactsRepository.create({
@@ -40,8 +40,35 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // atualiza registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    // verificação se o id existe
+    const contactExists = await contactsRepository.findById(id);
+    if (!contactExists) {
+      return response.status(400).json({ error: 'user not found' });
+    }
+
+    // verifica se o nome foi passado
+    if (!name) {
+      return response.status(400).json({ error: 'name is required' });
+    }
+
+    // verifica se e-mail ja existe em outro registro
+    const contactExistsbyEmail = await contactsRepository.findByEmail(email);
+    if (contactExistsbyEmail && contactExistsbyEmail.id !== id) {
+      return response.status(400).json({ error: 'this email is already in use' });
+    }
+
+    const contact = await contactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
@@ -51,7 +78,7 @@ class ContactController {
     const contact = await contactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'user not found' });
     }
 
     await contactsRepository.delete(id);
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,6 @@ router.get('/contacts', contactController.index);
 router.get('/contacts/:id', contactController.show);
 router.delete('/contacts/:id', contactController.delete);
 router.post('/contacts/', contactController.store);
+router.put('/contacts/:id', contactController.update);
 
 module.exports = router;
